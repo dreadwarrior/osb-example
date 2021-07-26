7f898a77b0bc0979f0ce8267cb3a7755ddcc0693
Thomas Juhnke <tommy@van-tomas.de>
2021-07-26 12:37:34 +0200

    Bootstrap Spring Boot application

---
+++ src/main/java/de/dreadlabs/osbexample/OsbExampleApplication.java
@@ -0,0 +1,13 @@
+package de.dreadlabs.osbexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsbExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OsbExampleApplication.class, args);
+    }
+
+}
